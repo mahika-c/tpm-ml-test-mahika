c6a62bb8a14109b0106e685265d5f9a5ffc33b59
mahika-c <msmahika.calyanakoti@gmail.com>
2024-02-28 23:49:22 -0500

    commit initial

--- feature_engine/features/sentiment_features.py
+++ feature_engine/features/sentiment_features.py
@@ -1,7 +1,26 @@
 # imports
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+
+MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 # TODO - DEFINE YOUR FEATURE EXTRACTOR HERE
 def get_sentiment(text):
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
     
+    sentiment = {}
+    for i in range(scores.shape[0]):
+        l = config.id2label[i]
+        sentiment[l] = float(scores[i])
+
+    return sentiment
+
     # sample output format
-    return({'positive': 0.0, 'negative': 0.0, 'neutral': 0.0})
+    # return({'positive': 0.0, 'negative': 0.0, 'neutral': 0.0})
--- feature_engine/featurize.py
+++ feature_engine/featurize.py
@@ -20,15 +20,15 @@ if __name__ == "__main__":
 	# FULL DATASETS BELOW
 
 	# Juries
-	jury_feature_builder = FeatureBuilder(
-	 	input_file_path = "../feature_engine/data/raw_data/jury_conversations_with_outcome_var.csv",
-	 	output_file_path_chat_level = "../feature_engine/output/jury_output_chat_level.csv",
-	 	output_file_path_conv_level = "../feature_engine/output/jury_output_conversation_level.csv"
-	 )
+	# jury_feature_builder = FeatureBuilder(
+	#  	input_file_path = "../feature_engine/data/raw_data/jury_conversations_with_outcome_var.csv",
+	#  	output_file_path_chat_level = "../feature_engine/output/jury_output_chat_level.csv",
+	#  	output_file_path_conv_level = "../feature_engine/output/jury_output_conversation_level.csv"
+	#  )
 
-	jury_feature_builder.featurize(col="message")
+	# jury_feature_builder.featurize(col="message")
 
-	# CSOP
+	# CSOP - Dataset I am using
 	csop_feature_builder = FeatureBuilder(
 		input_file_path = "../feature_engine/data/raw_data/csopII_conversations_withblanks.csv",
 		output_file_path_chat_level = "../feature_engine/output/csopII_output_chat_level.csv",
